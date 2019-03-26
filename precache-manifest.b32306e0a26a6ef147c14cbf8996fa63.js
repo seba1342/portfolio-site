875884007af76df71fba71cff5a679d4e81e5314
seba1342 <sbailouni@gmail.com>
2019-03-27 10:22:24 +1100

    Updates

---
+++ precache-manifest.b32306e0a26a6ef147c14cbf8996fa63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea85618c4c8be9d2492a",
+    "url": "/portfolio-site/static/js/runtime~main.7180bc28.js"
+  },
+  {
+    "revision": "16a4933eef4e873b6683",
+    "url": "/portfolio-site/static/js/main.28b425dc.chunk.js"
+  },
+  {
+    "revision": "625063f9a6f9b7b694f6",
+    "url": "/portfolio-site/static/js/2.baa180c0.chunk.js"
+  },
+  {
+    "revision": "16a4933eef4e873b6683",
+    "url": "/portfolio-site/static/css/main.dab8dcfa.chunk.css"
+  },
+  {
+    "revision": "ead185e270496b4b61132cc6709a32ae",
+    "url": "/portfolio-site/index.html"
+  }
+];
